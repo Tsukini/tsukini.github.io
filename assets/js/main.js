95e1e1e3ce17d698e6ff2fd26f147e661792263c
Gabriel Henrique Rudey <g_rudey@yahoo.com.br>
2015-02-22 13:11:32 -0300

    Adding pudding

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,3 +5,8 @@ var letterColors = [black, red, orange, green, blue, purple];
 drawName(name, letterColors);
 
 bounceBubbles();
+
+function pudding() {
+	$(".jumbo-canvas").css({opacity: '1', position: 'relative'})
+	$(".logo-container").addClass("hide")
+}
